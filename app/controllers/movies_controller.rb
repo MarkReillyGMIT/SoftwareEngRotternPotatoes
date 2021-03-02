f4bbb11ee4e31b4817ba66bea72f56cd9469c837
MARK REILLY <g00357230@gmit.ie>
2021-03-02 18:17:48 +0000

    Part 1 Complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,7 +7,15 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @movies = Movie.all
+    @all_ratings = Movie.all_ratings
+     
+    if params[:ratings].nil?
+      @ratings_to_show = []
+      @movies = Movie.with_ratings(params[:ratings])
+    else
+      @ratings_to_show = params[:ratings].keys
+      @movies = Movie.with_ratings(@ratings_to_show)
+    end
   end
 
   def new
